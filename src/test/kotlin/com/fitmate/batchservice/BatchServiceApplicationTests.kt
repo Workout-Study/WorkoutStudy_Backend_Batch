18ebbeb5634a8bee0c051078667ee9751baec16b
12OneTwo12 <jji042842@gmail.com>
2024-04-19 17:46:26 +0900

    delete test

--- src/test/kotlin/com/fitmate/batchservice/BatchServiceApplicationTests.kt
+++ src/test/kotlin/com/fitmate/batchservice/BatchServiceApplicationTests.kt
@@ -1,13 +1,8 @@
 package com.fitmate.batchservice
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class BatchServiceApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
 
 }
