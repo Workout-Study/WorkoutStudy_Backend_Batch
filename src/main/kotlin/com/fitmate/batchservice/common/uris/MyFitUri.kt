ca2c5951c482ea2cfb379f6c81439478d687c839
정정일 <105261146+12OneTwo12@users.noreply.github.com>
2024-08-13 18:09:15 +0900

    Update MyFitUri.kt

--- src/main/kotlin/com/fitmate/batchservice/common/uris/MyFitUri.kt
+++ src/main/kotlin/com/fitmate/batchservice/common/uris/MyFitUri.kt
@@ -3,9 +3,9 @@ package com.fitmate.batchservice.common.uris
 class MyFitUri {
 
     companion object {
-        const val FIT_GROUP_ROOT_URI = "http://fit-group:8081/my-fit-service"
+        const val MY_FIT_ROOT_URI = "http://my-fit:8081/my-fit-service"
 
-        const val FIT_CERTIFICATION_ROOT = "$FIT_GROUP_ROOT_URI/certifications"
+        const val FIT_CERTIFICATION_ROOT = "$MY_FIT_ROOT_URI/certifications"
         const val FIT_CERTIFICATION_PROGRESS = "$FIT_CERTIFICATION_ROOT/progresses"
     }
 }
